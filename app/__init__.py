aadc2901788dc9b6ca42d4649c20e665d866a1a6
Alexey Bolshakov <avbolshakov@gmail.com>
2023-12-12 11:45:47 +0300

    Чистый проект, создал основную структуру

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from config import Config
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Инициализируем тут расширения
+
+    # Регистрируем блюпринты
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    @app.route("/ping/")
+    def test_page():
+        return "pong!"
+    
+    return app
