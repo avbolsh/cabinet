208200c0fb9c472aa7e3109de3fb35e19dd5923f
Alexey Bolshakov <avbolshakov@gmail.com>
2023-12-13 11:45:32 +0300

    небольшие исправления в модели

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,9 @@ import uuid
 
 class User(UserMixin, db.Model):
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    snils = db.Column(db.String(14), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    snils = Column(String(14), index=True, unique=True)
+    email = Column(String(120), index=True, unique=True)
+    password_hash = Column(String(128))
     fullname = Column(String(100))
     first_name = Column(String(50))
     last_name = Column(String(50))
