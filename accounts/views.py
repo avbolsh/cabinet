20c1d49fede4c0d6c625f12a87a15d5c8b5ee26c
Alexey Bolshakov <avbolshakov@gmail.com>
2025-11-19 17:38:58 +0300

    Найтройка шаблов и статики
    Добавил базовый шаблон, подключил директорию с шаблонами, статикой.
    Добавил домашнюю страницу. Убрал ненужные view-функции из приложения
    accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,5 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Employee
-from .serializers import EmployeeSerializer
+from django.views.generic import TemplateView
 
 
-class EmployeeAPIView(APIView):
-    def post(self, request):
-        serializer = EmployeeSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class HomePageView(TemplateView):
+    template_name = 'index.html'
